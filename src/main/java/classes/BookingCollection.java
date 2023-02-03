080f58a2915eb48d60e99133263603bb7b813bf8
Lookatme5555 <nerdyboy55@gmail.com>
2023-02-03 19:21:19 +0800

    Added BookingcollectionTest

--- src/main/java/classes/BookingCollection.java
+++ src/main/java/classes/BookingCollection.java
@@ -1,5 +1,47 @@
 package classes;
 
-public class BookingCollection {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BookingCollection {
+	private ArrayList<Booking> bookings = new ArrayList<>();
+	private int capacity;
+
+//	public BookingCollection(ArrayList<Booking> bookings) {
+//		super();
+//		this.bookings = bookings;
+//		this.capacity = 5;
+//
+//	}
+	
+	public BookingCollection() {
+		this.capacity = 5;
+	}
+	
+	public BookingCollection(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public List<Booking> getbookings() {
+		return bookings;
+	}
+
+	public void setbookings(ArrayList<Booking> bookings) {
+		this.bookings = bookings;
+	}
+	
+	public void addbooking(Booking booking) {
+		if(bookings.size() != capacity) {
+			bookings.add(booking);
+		}
+	}
+	
+	public void editbooking(int index, Booking booking) {
+		bookings.set(index, booking);
+	}
+	
+	public void deletebooking(int index) {
+		bookings.remove(index);
+	}
+	
 }
